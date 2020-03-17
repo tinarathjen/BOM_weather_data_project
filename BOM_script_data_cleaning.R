a48d3edb1905e4c3cfae0af3a0d84601112cfce1
Tina Rathjen <tina.rathjen@csiro.au>
2020-03-17 14:39:02 +1100

    One solution to Question 1

--- BOM_script_data_cleaning.R
+++ BOM_script_data_cleaning.R
@@ -33,6 +33,40 @@ BOM_data
 Bom_data_grouped <- 
   Bom_data_sep_temp_withNA %>% group_by(Station_number)
 
+# create new variables removing NA values
+
+Bom_data_remove_NA <- Bom_data_grouped %>% 
+  filter(Rainfall!="NA") %>% 
+  filter(temp_min!="NA") %>% 
+  filter(temp_max!="NA")
+
+#or I can use the following
+Bom_data_remove_NA <- 
+  filter(Bom_data_grouped, Rainfall!="NA",
+         temp_min!="NA", temp_max!="NA")
+
+#new file with grouped by Station_number
+Bom_data_grouped<- 
+  Bom_data_remove_NA %>% group_by(Station_number)
+
+#Counting number of rows
+
+Bom_data_number_per_station <- 
+  summarise(Bom_data_grouped, Days_per_station=n())
+
+
+view(Bom_data_number_per_station)
+View(BOM_data)
+
+
+#an alternate method from Bill
+
+
+
+#bill also overwrites file name instead of
+
+
+
 
 
 
